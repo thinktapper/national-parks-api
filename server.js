859bfb9590146dbac0e9107a11ed3d6cb64dd90d
Andrew Tapper <andrew@tapper.codes>
2022-06-05 18:02:16 -0500

    add parks object

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 3000
+
+const parks = {
+    'Glacier':{
+        'location': 'Montana',
+        'date': 'May 11, 1910',
+        'size': 1013126,
+        'visitors': 3081656
+    },
+    'Zion':{
+        'location': 'Utah',
+        'date': 'November 19, 1919',
+        'size': 147243,
+        'visitors': 5039835
+    },
+    'Grand Canyon':{
+        'location': 'Arizona',
+        'date': 'February 26, 1919',
+        'size': 1201647,
+        'visitors': 4532677
+    },
+    'unknown':{
+        'location': 'unknown',
+        'date': 0,
+        'size': 0,
+        'visitors': 0
+    }
+}
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(PORT, ()=>{
+app.get('/api', (req,res)=>{
+    res.json(parks)
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is running on port ${PORT}, betta go catch it!`)
 })
